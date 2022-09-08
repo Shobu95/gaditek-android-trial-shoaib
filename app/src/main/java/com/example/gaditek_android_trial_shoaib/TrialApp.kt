0c90b8b2416d084bcbfe9db0dd5446af18b8fbd4
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-09-08 20:19:35 +0500

    Added hilt annotations to application class and main activity.

--- app/src/main/java/com/example/gaditek_android_trial_shoaib/TrialApp.kt
+++ app/src/main/java/com/example/gaditek_android_trial_shoaib/TrialApp.kt
@@ -1,6 +1,8 @@
 package com.example.gaditek_android_trial_shoaib
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class TrialApp: Application() {
 }
--- app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/MainActivity.kt
+++ app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.gaditek_android_trial_shoaib.ui.theme.GaditekandroidtrialshoaibTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
