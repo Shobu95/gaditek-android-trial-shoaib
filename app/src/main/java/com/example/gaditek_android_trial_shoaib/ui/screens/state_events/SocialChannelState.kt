9160df4c4d03cc6dfacbb1eac12144af4a8d0d92
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-09-13 13:05:34 +0500

    Renaming in SocialChannelState inProgress -> isLoading

--- app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/state_events/SocialChannelState.kt
+++ app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/state_events/SocialChannelState.kt
@@ -5,7 +5,7 @@ import com.example.gaditek_android_trial_shoaib.domain.model.SocialChannelModel
 data class SocialChannelState(
     val socialsList: List<SocialChannelModel> = emptyList(),
     val channelsList: List<SocialChannelModel> = emptyList(),
-    val inProgress: Boolean = false,
+    val isLoading: Boolean = false,
     val hasError: Boolean = false,
     val hasData: Boolean = false
 )
