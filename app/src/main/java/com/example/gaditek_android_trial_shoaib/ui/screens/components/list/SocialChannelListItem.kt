5bb7ad64afcf9476681216d4247a0cae949a4aa9
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-09-13 17:59:06 +0500

    Modified UI of SocialChannelListItem

--- app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/components/list/SocialChannelListItem.kt
+++ app/src/main/java/com/example/gaditek_android_trial_shoaib/ui/screens/components/list/SocialChannelListItem.kt
@@ -6,13 +6,12 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import com.example.gaditek_android_trial_shoaib.domain.model.SocialChannelModel
@@ -32,19 +31,19 @@ fun SocialChannelListItem(
         Row(
             modifier = Modifier
                 .padding(10.dp)
-                .fillMaxWidth()
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Image(
                 painter = rememberAsyncImagePainter(socialChannel.iconUrl),
                 contentDescription = "app icon",
                 modifier = Modifier
                     .padding(end = 14.dp, start = 6.dp)
-                    .size(50.dp)
-                    .clip(CircleShape)
+                    .size(60.dp)
             )
             Text(
                 text = socialChannel.name,
-                style = MaterialTheme.typography.body2,
+                style = MaterialTheme.typography.h6,
             )
         }
     }
