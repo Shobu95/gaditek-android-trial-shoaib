fd1346f6d9b7ddd21f0d33a18a357875854c9b91
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-09-13 11:48:24 +0500

    Updated/Added query function in Dao.

--- app/src/main/java/com/example/gaditek_android_trial_shoaib/data/local/database/SocialChannelDao.kt
+++ app/src/main/java/com/example/gaditek_android_trial_shoaib/data/local/database/SocialChannelDao.kt
@@ -1,7 +1,20 @@
 package com.example.gaditek_android_trial_shoaib.data.local.database
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.gaditek_android_trial_shoaib.data.local.entity.SocialChannelEntity
+import com.example.gaditek_android_trial_shoaib.domain.enums.AppType
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface SocialChannelDao {
+
+    @Query("SELECT * FROM social_channel_table WHERE type = :type")
+    fun getSocialChannels(type: AppType): Flow<List<SocialChannelEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(socialChannels: List<SocialChannelEntity>)
+
 }
